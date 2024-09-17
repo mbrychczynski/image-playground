c59b5280c13d20871bee80241ce290f4a2556869
Mateusz Brychczynski <mat.brych@gmail.com>
2024-09-17 10:22:53 +0100

    Playing with images

--- images.py
+++ images.py
@@ -1,2 +1,5 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 
+image = Image.open("./Pokedex/pikachu.jpg")
+filter_image = image.filter(ImageFilter.BLUR)
+filter_image.save("blur.png", "png")
