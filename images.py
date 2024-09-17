beee3149c56650f1adf9f2e5e182d2a608963611
Mateusz Brychczynski <mat.brych@gmail.com>
2024-09-17 10:28:44 +0100

    rotate, resize, crop

--- images.py
+++ images.py
@@ -1,5 +1,9 @@
 from PIL import Image, ImageFilter
 
 image = Image.open("./Pokedex/pikachu.jpg")
-filter_image = image.filter(ImageFilter.BLUR)
-filter_image.save("blur.png", "png")
+filter_image = image.convert("L")
+crooked = filter_image.rotate(180)
+resize = filter_image.resize((300, 300))
+box = (100, 100, 400, 400)
+region = filter_image.crop(box)
+region.save("region.png", "png")
