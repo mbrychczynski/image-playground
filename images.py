5c44e65151d4222e893f60338c3b41294362518d
Mateusz Brychczynski <mat.brych@gmail.com>
2024-09-17 10:34:10 +0100

    thumbnail

--- images.py
+++ images.py
@@ -1,9 +1,13 @@
 from PIL import Image, ImageFilter
 
-image = Image.open("./Pokedex/pikachu.jpg")
-filter_image = image.convert("L")
-crooked = filter_image.rotate(180)
-resize = filter_image.resize((300, 300))
-box = (100, 100, 400, 400)
-region = filter_image.crop(box)
-region.save("region.png", "png")
+# image = Image.open("./Pokedex/pikachu.jpg")
+# filter_image = image.convert("L")
+# crooked = filter_image.rotate(180)
+# resize = filter_image.resize((300, 300))
+# box = (100, 100, 400, 400)
+# region = filter_image.crop(box)
+# region.save("region.png", "png")
+
+img = Image.open("./astro.jpg")
+img.thumbnail((400, 400))
+img.save("new_astro.jpg")
